35ce4649df2f762b2c24b878166b37f561b2064f
韩嘉乐 <2382008060@qq.com>
2025-02-17 22:29:19 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -173,7 +173,7 @@ registerDownloadTask(
 // TODO: Remove in future
 registerDownloadTask(
     taskName = "downloadCompatKpatch",
-    srcUrl = "https://github.com/FrankHan052176/KernelPatch/releases/download/0.10.7/kpatch-android",
+    srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/0.10.7/kpatch-android",
     destPath = "${project.projectDir}/libs/arm64-v8a/libkpatch.so",
     project = project
 )
